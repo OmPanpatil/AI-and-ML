50c9bc95e0d58eb451099e01aaaa8e739677fc06
Om Panpatil <ompanpatil513@gmail.com>
2026-06-23 14:43:38 +0530

    python

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello, Mai🧿");
+# to ignore the line for points to be added for the coullegaes
+"""Too write big para we can use triple quotes and it will be ignored"""
+# TO store the data we use variable and labelling it with a name to assign the value.
+
+# Variables
+
+sher = 612
+print(sher)
